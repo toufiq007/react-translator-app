c1597e750d015e8d06c3a1cb77d2d45d5caa9deb
toufiq007 <t.limon2342@gmail.com>
2025-03-27 18:07:23 +0600

    complete the ui part and setup the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-
-import './App.css'
-import Translate from './components/Translate'
+import "./App.css";
+import Translate from "./components/Translate";
 
 function App() {
-
   return (
     <>
-      <Translate/>
+      <h1
+        className="font-bold text-center text-4xl font-mono"
+        style={{ padding: "10px 0" }}
+      >
+        Language translator
+      </h1>
+      <Translate />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Translate.tsx
+++ src/components/Translate.tsx
@@ -1,3 +1,5 @@
+import { Volume2, Copy, ArrowLeftRight } from "lucide-react";
+
 const Translate = () => {
   return (
     <div
@@ -15,8 +17,9 @@ const Translate = () => {
         style={{
           border: "2px solid #ccc",
           height: "300px",
-          borderRadius: "10px",
-          boxShadow: "0 10px 20px rgba(0,0,0,0.2)",
+          borderRadius: "10px 10px 0px 0px",
+          borderBottom: "none",
+          boxShadow: "0 10px 20px rgba(0,0,0,0.01)",
         }}
       >
         <div
@@ -26,7 +29,14 @@ const Translate = () => {
           <textarea
             spellCheck="false"
             placeholder="enter text"
-            style={{ width: "100%", height: "100%", padding: "5px" }}
+            style={{
+              width: "100%",
+              height: "100%",
+              padding: "5px",
+              resize: "none",
+              outline: "none",
+              border: "none",
+            }}
           ></textarea>
 
           <textarea
@@ -38,9 +48,51 @@ const Translate = () => {
               height: "100%",
               borderLeft: "2px solid #ccc",
               padding: "5px",
+              resize: "none",
+              outline: "none",
             }}
           ></textarea>
+          <div></div>
         </div>
+      </div>
+
+      <div
+        className="flex justify-between"
+        style={{ border: "1px solid #ccc", padding: "20px" }}
+      >
+        <div className="flex items-center gap-[20px]">
+          <Volume2 size="20px" />
+          <Copy size="20px" />
+          <select>
+            <option>limon</option>
+            <option>limon</option>
+            <option>limon</option>
+          </select>
+        </div>
+        <div>
+          <ArrowLeftRight size="20px" />
+        </div>
+        <div className="flex items-center gap-[20px]">
+          <Volume2 size="20px" />
+          <Copy size="20px" />
+          <select>
+            <option>limon</option>
+            <option>limon</option>
+            <option>limon</option>
+          </select>
+        </div>
+      </div>
+      <div style={{ marginTop: "20px" }}>
+        <button
+          style={{
+            width: "100%",
+            background: "rgb(248, 202, 186)",
+            padding: "10px",
+            borderRadius: "10px",
+          }}
+        >
+          Translate Text
+        </button>
       </div>
     </div>
   );
